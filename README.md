8365be4fb9ec7f90829de05df057d84293e44163
HokieGeek <andres.f.perez@gmail.com>
2019-09-06 09:55:01 -0400

    Finished the first stab of it, and polished it up

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # iq-license-report
+
+## Building
+
+You can either use the binaries that are - or should be - associated with the latest release on this GH page, or you can run `go build .`
+
+## Usage
+
+```sh
+./iq-license-report -appId <publicAppID>  [-iq http://<iqserver>:<iqport>] [-auth username:password] [-stage <iqstage>] [-file <filename>] [-serve <port>]
+```
+
+One of `-file` or `-serve` is required
+
+See the IQ documentation to find the application public ID from the GUI:
+<https://help.sonatype.com/iqserver/managing/application-management#ApplicationManagement-CopyingtheApplicationIDtoClipboard>
+
+Example of creating an HTML report as a file:
+
+```sh
+./iq-license-report -iq http://localhost:8070 -auth admin:admin123 -appId awesomeApp -stage release -file awesomeApp-licenses.html
+```
--- main.go
+++ main.go
@@ -10,33 +10,36 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	nexusiq "github.com/sonatype-nexus-community/gonexus/iq"
 )
 
-var pageTmpl = `<html>
-	<head>
-		<title>{{ .Title }}</title>
-	</head>
-	<body>
-		<ul>
-		{{ range .Licenses }}
-			<li>({{ .LicenseID }}) {{ .LicenseName }}</li>
-		{{ end }}
-		</ul>
-	</body>
-</html>`
-
 func writeHTML(w io.Writer, appID string, licenses []nexusiq.License) error {
 	page := struct {
-		Title    string
-		Licenses []nexusiq.License
+		Title, Created string
+		Licenses       []nexusiq.License
 	}{
 		Title:    fmt.Sprintf("Licenses for %s", appID),
 		Licenses: licenses,
+		Created:  time.Now().Format(time.RFC1123),
 	}
 
-	t := template.Must(template.New("page").Parse(pageTmpl))
+	t := template.Must(template.New("licenses").Parse(`
+	<html>
+		<head>
+			<title>{{ .Title }}</title>
+		</head>
+		<body>
+			<h1>{{ .Title }}</h1>
+			<ul>
+			{{ range .Licenses }}
+				<li>{{ .LicenseName }}</li>
+			{{ end }}
+			</ul>
+			<footer>Created {{ .Created }} </footer>
+		</body>
+	</html>`))
 	if err := t.Execute(w, page); err != nil {
 		return fmt.Errorf("could not generate page: %v", err)
 	}
@@ -60,17 +63,22 @@ func main() {
 		panic("appId is a required argument")
 	}
 
+	// Create gonexus IQ client
 	auth := strings.Split(*iqAuthPtr, ":")
 	iq, err := nexusiq.New(*iqServerPtr, auth[0], auth[1])
 	if err != nil {
 		panic(err)
 	}
 
+	// Retrieve the report struct based on appID and scan stage
 	report, err := nexusiq.GetRawReportByAppID(iq, *appIDPtr, *stagePtr)
 	if err != nil {
 		panic(err)
 	}
 
+	// Creates a string set of the declared and observed licenses from the report
+	// Declared License: these are the licenses that the developer of the component has identified.
+	// Observed License: these are the licenses that have been observed during Sonatype’s research.
 	licensesSet := make(map[nexusiq.License]struct{})
 	for _, c := range report.Components {
 		for _, l := range c.LicensesData.DeclaredLicenses {
@@ -81,15 +89,18 @@ func main() {
 		}
 	}
 
+	// Creates a slice ouf of the string set, and filters out noisy "licenses"
 	licenses := make([]nexusiq.License, 0, len(licensesSet))
 	for l := range licensesSet {
-		if l.LicenseID == "Not-Supported" {
+		switch l.LicenseID {
+		case "Not-Supported", "No-Source-License", "Not-Declared", "No-Sources", "See-License-Clause", "UNKNOWN":
 			continue
 		}
 		licenses = append(licenses, l)
 	}
 
 	switch {
+	// Serve the HTML page on the given port
 	case *servePortPtr != "":
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			if err := writeHTML(w, *appIDPtr, licenses); err != nil {
@@ -98,10 +109,10 @@ func main() {
 			}
 		})
 
+		log.Printf("Serving licenses report on port %s\n", *servePortPtr)
 		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", *servePortPtr), nil))
+	// Create the HTML page as a file
 	case *filePtr != "":
-		fmt.Println(report.Components[0].LicensesData)
-
 		fo, err := os.Create(*filePtr)
 		if err != nil {
 			panic(err)
@@ -115,8 +126,14 @@ func main() {
 
 		w := bufio.NewWriter(fo)
 
-		writeHTML(w, *appIDPtr, nil)
+		if err := writeHTML(w, *appIDPtr, licenses); err != nil {
+			panic(err)
+		}
+
+		if err = w.Flush(); err != nil {
+			panic(err)
+		}
 	default:
-		panic("One of file or serve must be used. See help")
+		panic("Missing required argument. One of 'file' or 'serve' must be used. See -help")
 	}
 }
